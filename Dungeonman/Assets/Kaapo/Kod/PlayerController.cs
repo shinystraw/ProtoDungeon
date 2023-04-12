11ba00637c5a3179d71361e136f23c37ae698ca3
SalmelaMikko <mikkosalmela2@kamk.fi>
2023-04-12 09:50:58 +0300

    GoblinPlaceholderAnimations

--- Dungeonman/Assets/Kaapo/Kod/PlayerController.cs
+++ Dungeonman/Assets/Kaapo/Kod/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     public float movementSpeed = 4; //movement speed in units per second
     public float sensitivity = 90; //sensitivity in degrees per second
     #endregion
+    Animator pAnimator;
+    bool pMoving;
 
     #region Dynamic Variables
 
@@ -16,7 +18,8 @@ public class PlayerController : MonoBehaviour
     public bool movementMode;
     void Start()
     {
-        
+        pAnimator = gameObject.GetComponent<Animator>();
+        pMoving = false;
     }
 
     void Update()
@@ -40,5 +43,19 @@ public class PlayerController : MonoBehaviour
             transform.position = new Vector3(transform.position.x + movementSpeed * horizontalInput * Time.deltaTime, transform.position.y + movementSpeed * verticalInput * Time.deltaTime);
         }
         #endregion
+        if (Input.GetKeyDown(KeyCode.R))
+            pMoving = true;
+        else
+            pMoving = false;
+
+
+        if (pMoving == true)
+            pAnimator.SetBool("PlayerMove", true);
+        Debug.Log("Bool is true");
+
+        if (pMoving == false)
+            pAnimator.SetBool("PlayerMove", false);
+        Debug.Log("Bool is false");
+
     }
 }
